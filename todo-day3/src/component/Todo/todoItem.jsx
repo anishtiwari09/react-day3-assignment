a9db05b0baa8e6a57266dd71e7007eb1217e7b73
anishtiwari09 <aneeshtiwari24@gmail.com>
2021-11-18 18:49:55 +0530

    todo list-key

--- todo-day3/src/component/Todo/todoItem.jsx
+++ todo-day3/src/component/Todo/todoItem.jsx
@@ -4,7 +4,7 @@ export default function TodoItem({ data, handle, handleDelete, handleTogle }) {
   return (
     <ul>
       {data.map((item) => (
-        <li>
+        <li key={item.id}>
           {item.title} - {item.description} - {item.status ? "Done" : "Pending"}
           <Button title="Toggle" onClick={() => handleTogle(item.id)} />
           <Button title="X" onClick={() => handleDelete(item.id)} />
