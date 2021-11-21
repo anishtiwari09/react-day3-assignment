e2524438f48710a07a254ecda95879ed3518140c
anishtiwari09 <aneeshtiwari24@gmail.com>
2021-11-21 11:32:04 +0530

    filter todo

---
+++ filterTodo/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Todo from "./component/Todo/Todo";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
+}
---
+++ filterTodo/src/component/Todo/header.jsx
@@ -0,0 +1,7 @@
+export default function Header({ title }) {
+  return (
+    <>
+      <h3>{title}</h3>
+    </>
+  );
+}
